1324196f80a763eb2908d95b52bca04a1fad6c2b
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2024-05-30 23:56:04 +0300

    specifies commands for stopping and telling time

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,81 @@
 <script setup>
-import {ref, onMounted} from 'vue';
-
+import { ref, onMounted } from 'vue'
 const transcript = ref('')
-const isRecording= ref(false);
+const isRecording = ref(false)
 
-const Recognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const sr = new Recognition();
+const Recognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const sr = new Recognition()
 
-onMounted(()=>{
-  sr.continuous = true;
-  sr.interimResults= true;
+onMounted(() => {
+	sr.continuous = true
+	sr.interimResults = true
 
-  sr.onstart = ()=>{
-    console.log('SR has started')
-    isRecording.value = true;
+	sr.onstart = () => {
+		console.log('SR Started')
+		isRecording.value = true
+	}
 
-  }
-  sr.onend = () => {
-    console.log("SR has stopped")
-    isRecording.value= false;
-  }
-  sr.onresult = (evt) =>{
-    console.log(evt)
-  }
+	sr.onend = () => {
+		console.log('SR Stopped')
+		isRecording.value = false
+	}
+
+	sr.onresult = (evt) => {
+		for (let i = 0; i < evt.results.length; i++) {
+			const result = evt.results[i]
+
+			if (result.isFinal) CheckForCommand(result)
+		}
+
+		const t = Array.from(evt.results)
+			.map(result => result[0])
+			.map(result => result.transcript)
+			.join('')
+		
+		transcript.value = t
+	}
 })
-const ToggleMic = () =>{
-  if(isRecording.value){
-    sr.stop()
-  } else{
-    sr.start()
-  }
+
+const CheckForCommand = (result) => {
+	const t = result[0].transcript;
+	if (t.includes('stop recording')) {
+		sr.stop()
+	} else if (
+		t.includes('what is the time') ||
+		t.includes('what\'s the time')
+	) {
+		sr.stop()
+		alert(new Date().toLocaleTimeString())
+		setTimeout(() => sr.start(), 100)
+	}
 }
 
+const ToggleMic = () => {
+	if (isRecording.value) {
+		sr.stop()
+	} else {
+		sr.start()
+	}
+}
 </script>
 
 <template>
-  <div>
-      <button :class="`mic`" @click="ToggleMic" >Recording</button>
-      <div class="transcript"></div>
- </div>
+	<div class="app">
+		<button :class="`mic`" @click="ToggleMic">Record</button>
+		<div class="transcript" v-text="transcript"></div>
+	</div>
 </template>
 
 <style>
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Fira sans', sans-serif;
 }
-body{
-  background: rgb(55, 44, 57);
-  color: #fff;
+
+body {
+	background: #281936;
+	color: #FFF;
 }
 </style>
